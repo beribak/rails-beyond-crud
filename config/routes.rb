db612d74a039c5c9dd6e1f45827220995e9ce839
danko beribak <beribakdanko@gmail.com>
2022-11-16 10:33:49 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :reviews, only: [:destroy]
+  resources :restaurants do  # ,  only: [:show, :index, :new]
+    
+    resources :reviews, only: [:new, :create]
+    
+    collection do
+      get :top
+    end
+    
+    member do
+      get :chef
+    end
+  end
+end
